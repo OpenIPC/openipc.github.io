3de3d3990db0a80dc9e2e084aa5b53e4f68f8310
Igor Zalatov <flyrouter@gmail.com>
2021-05-22 10:41:32 +0300

    Update logo

---
+++ docs/script/convert_logos.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# More info:
+# http://najomi.org/_nix/imagemagick
+
+
+cd ../images
+#
+for f in partner_*_orig.png; do echo $f; $(convert $f -resize x100 ${f%_orig.png}_mini.png); done
+#
+montage -geometry +30+30 -tile 4 partner_*_mini.png partners_all.png
+#
+cd -
+#
